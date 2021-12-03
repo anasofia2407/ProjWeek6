fe71d670426b8675791ab4f122abf3a6bbee6f94
Ana Sofia Costa <up202007602@fe.up.pt>
2021-12-03 14:36:19 +0000

    inicio do novo projeto

---
+++ src/main/java/stringDrink.java
@@ -0,0 +1,14 @@
+
+class StringDrink{
+    private String word;
+    StringDrink(String word){
+        this.word=word;
+    }
+    public String getText(){
+        return this.word;
+    }
+    public void setText(String word){
+        this.word=word;
+    }
+}
+
